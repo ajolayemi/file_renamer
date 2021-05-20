9fa5f10879ab88d0e4038340800a80db1ce43080
jolayemi_local <ajolayemi82@gmail.com>
2021-05-20 20:53:33 +0200

    Defined loadFiles module

--- rprename/views.py
+++ rprename/views.py
@@ -29,3 +29,22 @@ class Window(QWidget, Ui_Window):
 
     def _setupUI(self):
         self.setupUi(self)
+
+    def loadFiles(self):
+        self.dstFileList.clear()
+        if self.dirEdit.text():
+            initDir = self.dirEdit.text()
+        else:
+            initDir = str(Path.home())
+            files, filter_ = QFileDialog.getOpenFileNames(
+                self, 'Choose Files to Rename', initDir, filter=FILTERS
+            )
+            if len(files) > 0:
+                fileExtension = filter_[filter_.index('*'): -1]
+                self.extensionLabel.setText(fileExtension)
+                srcDirName = str(Path(files[0]).parent)
+                self.dirEdit.setText(srcDirName)
+                for file in files:
+                    self._files.append(Path(file))
+                    self.srcFileList.addItem(file)
+                self._filesCount = len(self._files)
